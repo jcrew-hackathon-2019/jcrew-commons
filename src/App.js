63eb0c4362992a65c4a05b6a83b321cce572093f
dspencer926 <dspencer926@gmail.com>
2019-02-21 12:04:09 -0500

    Making sure sass works

--- src/App.js
+++ src/App.js
@@ -4,10 +4,25 @@ import './app.scss';
 import Sidebar from './components/Sidebar'
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      section: 'welcome',
+    }
+  }
+
+  sectionChange = (section) => this.setState({ section })
+
   render() {
+
+    // const body = {
+
+    // }
+
     return (
       <main>
         <Sidebar />
+        {/* {body} */}
       </main>
     );
   }
