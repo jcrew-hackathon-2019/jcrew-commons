f08bfa499d1085fd9e79115181b76c3976c814cb
Clay <clay.reinken@jcrew.com>
2019-02-22 12:09:32 -0500

    prism theme

---
+++ src/assets/js/prism-commons.js
@@ -0,0 +1,215 @@
+export default {
+    "code[class*=\"language-\"]": {
+        "fontFamily": "Consolas, Menlo, Monaco, \"Andale Mono WT\", \"Andale Mono\", \"Lucida Console\", \"Lucida Sans Typewriter\", \"DejaVu Sans Mono\", \"Bitstream Vera Sans Mono\", \"Liberation Mono\", \"Nimbus Mono L\", \"Courier New\", Courier, monospace",
+        "fontSize": "14px",
+        "lineHeight": "1.375",
+        "direction": "ltr",
+        "textAlign": "left",
+        "whiteSpace": "pre",
+        "wordSpacing": "normal",
+        "wordBreak": "normal",
+        "MozTabSize": "4",
+        "OTabSize": "4",
+        "tabSize": "4",
+        "WebkitHyphens": "none",
+        "MozHyphens": "none",
+        "msHyphens": "none",
+        "hyphens": "none",
+        "background": "#FFF",
+        "color": "#00CD4F"
+    },
+    "pre[class*=\"language-\"]": {
+        "fontFamily": "Consolas, Menlo, Monaco, \"Andale Mono WT\", \"Andale Mono\", \"Lucida Console\", \"Lucida Sans Typewriter\", \"DejaVu Sans Mono\", \"Bitstream Vera Sans Mono\", \"Liberation Mono\", \"Nimbus Mono L\", \"Courier New\", Courier, monospace",
+        "fontSize": "14px",
+        "lineHeight": "1.375",
+        "direction": "ltr",
+        "textAlign": "left",
+        "whiteSpace": "pre",
+        "wordSpacing": "normal",
+        "wordBreak": "normal",
+        "MozTabSize": "4",
+        "OTabSize": "4",
+        "tabSize": "4",
+        "WebkitHyphens": "none",
+        "MozHyphens": "none",
+        "msHyphens": "none",
+        "hyphens": "none",
+        "background": "#FFF",
+        "color": "#00CD4F",
+        "padding": "1em",
+        "margin": ".5em 0",
+        "overflow": "auto"
+    },
+    "pre[class*=\"language-\"]::-moz-selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "pre[class*=\"language-\"] ::-moz-selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "code[class*=\"language-\"]::-moz-selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "code[class*=\"language-\"] ::-moz-selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "pre[class*=\"language-\"]::selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "pre[class*=\"language-\"] ::selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "code[class*=\"language-\"]::selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    "code[class*=\"language-\"] ::selection": {
+        "textShadow": "none",
+        "background": "#6a51e6"
+    },
+    ":not(pre) > code[class*=\"language-\"]": {
+        "padding": ".1em",
+        "borderRadius": ".3em"
+    },
+    "comment": {
+        "color": "#000"
+    },
+    "prolog": {
+        "color": "#000"
+    },
+    "doctype": {
+        "color": "#000"
+    },
+    "cdata": {
+        "color": "#000"
+    },
+    "punctuation": {
+        "color": "#000"
+    },
+    "namespace": {
+        "Opacity": ".7"
+    },
+    "tag": {
+        "color": "#e09142"
+    },
+    "operator": {
+        "color": "#e09142"
+    },
+    "number": {
+        "color": "#e09142"
+    },
+    "property": {
+        "color": "#00CD4F"
+    },
+    "function": {
+        "color": "#00CD4F"
+    },
+    "tag-id": {
+        "color": "#eeebff"
+    },
+    "selector": {
+        "color": "#eeebff"
+    },
+    "atrule-id": {
+        "color": "#eeebff"
+    },
+    "code.language-javascript": {
+        "color": "#c4b9fe"
+    },
+    "attr-name": {
+        "color": "#c4b9fe"
+    },
+    "code.language-css": {
+        "color": "#0076FF"
+    },
+    "code.language-scss": {
+        "color": "#0076FF"
+    },
+    "boolean": {
+        "color": "#0076FF"
+    },
+    "string": {
+        "color": "#0076FF"
+    },
+    "entity": {
+        "color": "#0076FF",
+        "cursor": "help"
+    },
+    "url": {
+        "color": "#0076FF"
+    },
+    ".language-css .token.string": {
+        "color": "#0076FF"
+    },
+    ".language-scss .token.string": {
+        "color": "#0076FF"
+    },
+    ".style .token.string": {
+        "color": "#0076FF"
+    },
+    "attr-value": {
+        "color": "#0076FF"
+    },
+    "keyword": {
+        "color": "#0076FF"
+    },
+    "control": {
+        "color": "#0076FF"
+    },
+    "directive": {
+        "color": "#0076FF"
+    },
+    "unit": {
+        "color": "#0076FF"
+    },
+    "statement": {
+        "color": "#0076FF"
+    },
+    "regex": {
+        "color": "#0076FF"
+    },
+    "atrule": {
+        "color": "#0076FF"
+    },
+    "placeholder": {
+        "color": "#0076FF"
+    },
+    "variable": {
+        "color": "#0076FF"
+    },
+    "deleted": {
+        "textDecoration": "line-through"
+    },
+    "inserted": {
+        "borderBottom": "1px dotted #eeebff",
+        "textDecoration": "none"
+    },
+    "italic": {
+        "fontStyle": "italic"
+    },
+    "important": {
+        "fontWeight": "bold",
+        "color": "#c4b9fe"
+    },
+    "bold": {
+        "fontWeight": "bold"
+    },
+    "pre > code.highlight": {
+        "Outline": ".4em solid #8a75f5",
+        "OutlineOffset": ".4em"
+    },
+    ".line-numbers .line-numbers-rows": {
+        "borderRightColor": "#2c2937"
+    },
+    ".line-numbers-rows > span:before": {
+        "color": "#3c3949"
+    },
+    ".line-highlight": {
+        "background": "linear-gradient(to right, rgba(224, 145, 66, 0.2) 70%, rgba(224, 145, 66, 0))"
+    }
+}
